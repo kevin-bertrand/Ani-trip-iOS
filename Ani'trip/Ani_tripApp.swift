bf5d50e671197b817eb53872bfd57f6fde6902fb
Kevin Bertrand <k.bertrand@desyntic.com>
2022-07-04 13:21:10 +0200

    Initial Commit

---
+++ Ani'trip/Ani_tripApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ani_tripApp.swift
+//  Ani'trip
+//
+//  Created by Kevin Bertrand on 04/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct Ani_tripApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
